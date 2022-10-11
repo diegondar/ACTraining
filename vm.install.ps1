91ff960ca8a39dee5868d33c9aacedaeea82625c
diegondar <59311217+diegondar@users.noreply.github.com>
2022-10-11 18:03:18 -0300

    Update vm.install.ps1

--- vm.install.ps1
+++ vm.install.ps1
@@ -1,5 +1,3 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) -y
-
 choco install office365proplus -y
 choco install nodejs -y
 choco install javaruntime -y
